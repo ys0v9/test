e84435a707518ff072dc77035a64efff39e3fb3b
ys0v9 <sua9097@gmail.com>
2024-11-27 19:58:36 +0900

    241127-list

--- s.py
+++ s.py
@@ -1,3 +1,36 @@
-print(1)
-print(2)
-print(3)
+# a=[11111,22222]
+# a.append(3)
+# a.insert(2,1)
+# print(a)
+# del a[0]
+# print(a)
+# a.remove(22222)
+# print(a)
+# print(a.count(1))
+# b="abb11 c"
+# print(b.count('b'))
+
+# a=[50,10,30,40,15]
+# a.sort() #오름차순
+# print(a)
+# a.sort(reverse=True) #내림차순
+# b=sorted(a) #오름차순
+# c=sorted(a,reverse=True) #내림차순
+# print(a)
+# print(b)
+# print(c)
+
+# a=[[13,14],[10,20],[30,50,90]]
+# print(a[0][1])
+
+# a=[[13,14],[10,20],[30,50,90]]
+# sorted_a=sorted(a,key=lambda x:x[1]) #reverse=True
+# print(sorted_a)
+
+# a=[10,20,30,40,50]
+# for i in range(0,len(a),1):
+#     print(a[i])
+
+# a=[10,20,30,40,50]
+# for listdata in a:
+#     print(listdata)
