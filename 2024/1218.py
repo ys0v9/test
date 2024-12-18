3f30ea464671d34aeddaad0276030984da40aa8c
ys0v9 <sua9097@gmail.com>
2024-12-18 19:24:09 +0900

    241218

---
+++ 2024/1218.py
@@ -0,0 +1,61 @@
+# a={10,30,20,10,20,10,10} #{} << 오름차순으로 정렬, 중복 데이터는 1개로 취급
+# print(a)
+# a.add(50)
+# a.add(20) #중복되는 데이터라 add해도 변함 없음
+# print(a)
+# a.remove(20)
+# print(a)
+# print(len(a))
+# print(max(a))
+# print(min(a))
+# print(sum(a))
+
+# a={10,20,30}
+# b={30,40,50}
+# print(a.union(b)) #합집합
+# print(a|b)
+# print(a.difference(b)) #차집합
+# print(a-b)
+# print(b.difference(a))
+# print(b-a)
+# print(a.intersection(b)) #교집합
+# print(a&b)
+
+# a={10,20,30}
+# for i in a:
+#     print(i)
+
+# a={1:'one','2':2,3.0:'three'}
+# print(a)
+
+# book={'수학':5, '영어':10, '파이썬':3}
+# print(book)
+# print(book['수학'])
+# book['스크래치']=100
+# print(book)
+# book['영어']=90
+# print(book)
+# book['영어']+=100
+# print(book)
+# del book['수학']
+# print(book)
+
+# book={'수학':5, '영어':10, '파이썬':3}
+# print(book)
+# print(len(book))
+# for i in book:
+#     print(i,book[i])
+# a=book.keys()
+# for i in book.keys():
+#     print(i)
+# value=list(book.values())
+# print(value)
+# item=list(book.items())
+# print(item)
+
+# book = {'수학': 5, '영어': 10, '파이썬': 3}
+# i,j={'수힉',5}
+
+book = {'수학': 5, '영어': 10, '파이썬': 3}
+for k,v in book.items():
+    print(k,v)
